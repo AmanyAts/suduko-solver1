9c25b6b5f451a4fbab115d0ebd25e7fdcf2f35d3
Amany Alattas <amanyats@Amanys-MacBook-Air.local>
2021-07-31 16:29:46 +0300

    update unitlist

--- solution.py
+++ solution.py
@@ -2,17 +2,14 @@
 from utils import *
 
 
+# Encode the board: units and peers
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
-square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
-
-# TODO: Update the unit list to add the new diagonal units
-diagonal_lr = [rows[i] + cols[i] for i in range(len(rows))]
-rcols = cols[::-1]
-diagonal_rl = [rows[i] + rcols[i] for i in range(len(rows) -1, -1, -1)]
-unitlist.append(diagonal_lr)
-unitlist.append(diagonal_rl)
+square_units = [cross(rs, cs) for rs in ("ABC","DEF","GHI") for cs in ("123","456","789")]
+diagonal_units = [[rows[i] + cols[i] for i in range(9)]] + [[rows[i] + cols[::-1][i] for i in range(9)]]
+unitlist = row_units + column_units + square_units + diagonal_units
+units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
+peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 # unitlist = unitlist
 
 # Must be called after all units (including diagonals) are added to the unitlist
